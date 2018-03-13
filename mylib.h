6f6f807963b372026693a9d3ff7545b159106e79
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-03-13 14:23:17 -0400

    added some things we will need

--- mylib.h
+++ mylib.h
@@ -1,6 +1,11 @@
 #ifndef mylib_h
 #define mylib_h
 
+#include <sys/types.h>
 
+#define malloc(x) myallocate(x, __FILE__, __LINE__, THREADREQ)
+#define free(x) mydeallocate(x, __FILE__, __LINE__, THREADREQ)
+
+void * myallocate(size_t size);
 
 #endif
