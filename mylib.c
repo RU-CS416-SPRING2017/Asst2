ee88a8c9c0f3caa2db74aa27cf6fa667bb65375e
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-03-13 18:56:46 -0400

    fix cast

--- mylib.c
+++ mylib.c
@@ -3,6 +3,7 @@
 // 8 * 1024 * 1024 = 2^23
 #define MEMORY_SIZE 8388608
 #define METADATA_SIZE sizeof(struct memoryMetadata)
+// Block size of block with payload x
 #define BLOCK_SIZE(x) x + (METADATA_SIZE * 2)
 
 #define CHAR_PTR(x) ((char *) x)
@@ -20,7 +21,7 @@ void * myallocate(size_t size, char * fileName, int lineNumber, int requester) {
     struct memoryMetadata * head = META_PTR(memory);
     
     while (head->used) {
-        head = CHAR_PTR(head) + BLOCK_SIZE(head->size);
+        head = META_PTR(CHAR_PTR(head) + BLOCK_SIZE(head->size));
     }
 
     if (((CHAR_PTR(head) - memory) + BLOCK_SIZE(size)) > MEMORY_SIZE) {
