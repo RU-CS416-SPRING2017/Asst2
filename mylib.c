d5175b4de9472d43ccb7965177d7fa8279195327
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-03-14 18:36:55 -0400

    added partitioning, no test

--- mylib.c
+++ mylib.c
@@ -4,16 +4,13 @@
 #define MEM_SIZE (8 * 1024 * 1024)
 #define BLK_META_SIZE sizeof(struct blockMetadata)
 #define DBL_BLK_META_SIZE (BLK_META_SIZE * 2)
+#define MEM_META_SIZE sizeof(struct memoryMetadata)
 #define BLK_SIZE(x) (x + DBL_BLK_META_SIZE) // x is payload size of block
 
 // Casting macros
 #define CHAR_PTR(x) ((char *) (x))
 #define BLK_META_PTR(x) ((struct blockMetadata *) (x))
 #define MEM_META_PTR(x) ((struct memoryMetadata *) (x))
-
-// Macros to metadata ends of the memory
-#define MEMORY_HEAD BLK_META_PTR(memory)
-#define MEMORY_TAIL BLK_META_PTR(memory + MEM_SIZE - BLK_META_SIZE)
 
 // These macros determine how much of memory should
 // be partitioned for the thread library vs threads
@@ -36,12 +33,12 @@ struct memoryPartition {
 // Metadata for memory
 struct memoryMetadata {
     int initialized;
-    struct memoryPartition libraryMemoryPartition;
-    struct memoryPartition threadsMemoryPartition;
+    struct memoryPartition library;
+    struct memoryPartition threads;
 };
 
 // "Main memory"
-char memory[MEM_SIZE];
+char memory[MEM_SIZE] = { 0 };
 
 // Returns the tail of a block whose initialized head is given
 struct blockMetadata * getTail(struct blockMetadata * head) {
@@ -78,13 +75,13 @@ void setBlockMetadata(void * block, int used, size_t payloadSize) {
 // Allocates memory of size between firstHead and lastTail.
 // Returns 0 if no space available, else returns pointer
 // to allocated memory.
-void * allocateBetween(size_t size, struct blockMetadata * firstHead, struct blockMetadata * lastTail) {
+void * allocateFrom(size_t size, struct memoryPartition * partition) {
 
-    struct blockMetadata * head = firstHead;
+    struct blockMetadata * head = partition->firstHead;
     
     while (head->used || size > head->payloadSize) {
         head = BLK_META_PTR(CHAR_PTR(head) + BLK_SIZE(head->payloadSize));
-        if ((head - 1) == lastTail) { return 0; }
+        if ((head - 1) ==partition->lastTail) { return 0; }
     }
 
     if ((size + DBL_BLK_META_SIZE) >= head->payloadSize) {
@@ -100,45 +97,73 @@ void * allocateBetween(size_t size, struct blockMetadata * firstHead, struct blo
 }
 
 // Allocates size bytes in memory and returns a pointer to it
-void * myallocate(size_t size, char * fileName, int lineNumber, int requester) {
+void * myallocate(size_t size, char * fileName, int lineNumber, int request) {
 
-    struct memoryMetadata h;
+    struct memoryMetadata * memoryInfo = MEM_META_PTR(memory);
 
-    if (MEMORY_HEAD->payloadSize == 0) {
-        setBlockMetadata(memory, 0, MEM_SIZE - DBL_BLK_META_SIZE);
+    if (!(memoryInfo->initialized)) {
+        
+        size_t spaceLeft = MEM_SIZE - MEM_META_SIZE;
+        size_t numDiv = LIBRARY_MEMORY_WEIGHT + THREADS_MEMORY_WEIGHT;
+        size_t divSize = spaceLeft / numDiv;
+
+        size_t threadsPartitionSize = divSize * THREADS_MEMORY_WEIGHT;
+        size_t threadsPayloadSize = threadsPartitionSize - DBL_BLK_META_SIZE;
+        size_t libraryPartitionSize = spaceLeft - threadsPartitionSize;
+        size_t libraryPayloadSize = libraryPartitionSize - DBL_BLK_META_SIZE;
+
+        struct blockMetadata * libraryHead = BLK_META_PTR(memoryInfo + 1);
+        setBlockMetadata(libraryHead, 0, libraryPayloadSize);
+        struct blockMetadata * libraryTail = getTail(libraryHead);
+
+        struct blockMetadata * threadsHead = libraryTail + 1;
+        setBlockMetadata(threadsHead, 0, threadsPayloadSize);
+        struct blockMetadata * threadsTail = getTail(threadsHead);
+
+        memoryInfo->library.firstHead = libraryHead;
+        memoryInfo->library.lastTail = libraryTail;
+        memoryInfo->threads.firstHead = threadsHead;
+        memoryInfo->threads.lastTail = threadsTail;
     }
-    
-    return allocateBetween(size, MEMORY_HEAD, MEMORY_TAIL);
+
+    if (request == LIBRARYREQ) { return allocateFrom(size, &(memoryInfo->library)); }
+    else if (request == THREADREQ) { return allocateFrom(size, &(memoryInfo->threads)); }
+    else { return 0; }
 }
 
 // Deallocates a block between firstHead and lastTail where the payload is refrenced by ptr
-void deallocateBetween(void * ptr, struct blockMetadata * firstHead, struct blockMetadata * lastTail) {
+void deallocateFrom(void * ptr, struct memoryPartition * partition) {
 
-    struct blockMetadata * head = BLK_META_PTR(ptr) - 1;
-    struct blockMetadata * tail = getTail(head);
-    
-    if (head != firstHead) {
-        struct blockMetadata * previousTail = head - 1;
-        if (previousTail->used == 0) {
-            size_t newPayloadSize = head->payloadSize + previousTail->payloadSize + DBL_BLK_META_SIZE;
-            head = getHead(previousTail);
-            setBlockPayloadSize(head, newPayloadSize);
+    if (ptr >= partition->firstHead && ptr < partition->lastTail) {
+
+        struct blockMetadata * head = BLK_META_PTR(ptr) - 1;
+        struct blockMetadata * tail = getTail(head);
+        
+        if (head != partition->firstHead) {
+            struct blockMetadata * previousTail = head - 1;
+            if (previousTail->used == 0) {
+                size_t newPayloadSize = head->payloadSize + previousTail->payloadSize + DBL_BLK_META_SIZE;
+                head = getHead(previousTail);
+                setBlockPayloadSize(head, newPayloadSize);
+            }
         }
-    }
 
-    if (tail != lastTail) {
-        struct blockMetadata * nextHead = tail + 1;
-        if (nextHead->used == 0) {
-            size_t newPayloadSize = tail->payloadSize + nextHead->payloadSize + DBL_BLK_META_SIZE;
-            tail = getTail(nextHead);
-            setBlockPayloadSize(head, newPayloadSize);
+        if (tail != partition->lastTail) {
+            struct blockMetadata * nextHead = tail + 1;
+            if (nextHead->used == 0) {
+                size_t newPayloadSize = tail->payloadSize + nextHead->payloadSize + DBL_BLK_META_SIZE;
+                tail = getTail(nextHead);
+                setBlockPayloadSize(head, newPayloadSize);
+            }
         }
-    }
 
-    setBlockUsed(head, 0);
+        setBlockUsed(head, 0);
+    }
 }
 
 // Frees memory refrenced by ptr that was previously allocated with myallocate
 void mydeallocate(void * ptr, char * fileName, int lineNumber, int request) {
-    deallocateBetween(ptr, MEMORY_HEAD, MEMORY_TAIL);
+    struct memoryMetadata * memoryInfo = MEM_META_PTR(memory);
+    if (request == LIBRARYREQ) { deallocateFrom(ptr, &(memoryInfo->library)); }
+    else if (request == THREADREQ) { deallocateFrom(ptr, &(memoryInfo->threads)); }
 }
