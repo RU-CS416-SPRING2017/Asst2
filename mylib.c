2fbd5602c2afe16a83f8a48d092b85a9ecbfd5e8
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2018-03-13 23:25:56 -0400

    nicer to get head from tail and vice versa

--- mylib.c
+++ mylib.c
@@ -12,12 +12,22 @@
 // Metadata for a block of memory.
 // Used in head and tail.
 struct memoryMetadata {
-    size_t size;
+    size_t payloadSize;
     int used;
 };
 
 // "Main memory"
 char memory[MEMORY_SIZE];
+
+// Returns the tail of a block whose initialized head is given
+struct memoryMetadata * getTail(struct memoryMetadata * head) {
+    return META_PTR(CHAR_PTR(head) + METADATA_SIZE + head->payloadSize);
+}
+
+// Returns the head of a block whose initialized tail is given
+struct memoryMetadata * getHead(struct memoryMetadata * tail) {
+    return META_PTR(CHAR_PTR(tail) - METADATA_SIZE - tail->payloadSize);
+}
 
 // Allocates size bytes in memory and returns a pointer to it
 void * myallocate(size_t size, char * fileName, int lineNumber, int requester) {
@@ -25,7 +35,7 @@ void * myallocate(size_t size, char * fileName, int lineNumber, int requester) {
     struct memoryMetadata * head = META_PTR(memory);
     
     while (head->used) {
-        head = META_PTR(CHAR_PTR(head) + BLOCK_SIZE(head->size));
+        head = META_PTR(CHAR_PTR(head) + BLOCK_SIZE(head->payloadSize));
     }
 
     if (((CHAR_PTR(head) - memory) + BLOCK_SIZE(size)) > MEMORY_SIZE) {
@@ -33,10 +43,8 @@ void * myallocate(size_t size, char * fileName, int lineNumber, int requester) {
     }
 
     head->used = 1;
-    head->size = size;
-
-    struct memoryMetadata * tail = META_PTR(CHAR_PTR(head) + METADATA_SIZE + size);
-    *tail = *head;
+    head->payloadSize = size;
+    *getTail(head) = *head;
 
     return CHAR_PTR(head) + METADATA_SIZE;
 }
